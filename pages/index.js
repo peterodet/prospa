3ffd8523e8a1bd854afb6cebcc53adc25c647297
Peter Odetayo <peterodet@outlook.com>
2021-05-20 17:25:00 +0100

    Fixes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Layout from '../layouts/auth'
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 import { useRouter } from 'next/router'
 import Button from '../components/button'
 import Input from '../components/input'
@@ -19,37 +19,39 @@ export default function Home() {
   }
 
   return (
-    <Layout title="Sign Up" bg="bg-purple-600">
-      <div>
-        <div className="flex flex-col max-w-375 mx-auto">
-          <h4 className="text-2.5xl font-medium leading-tight mb-2 md:mb-2">
-            Create your account
-          </h4>
-          <p className="text-md text-gray-400">A short description about account types</p>
-          <form onSubmit={(e) => submit(e)} className="flex flex-col gap-6 mt-8">
-            <div>
-              <Input label="First name" />
-            </div>
-            <div>
-              <Input label="Last name" />
-            </div>
-            <div className="grid grid-cols-12 gap-3">
-              <div className="col-span-4">
-                <Select label="Country" block={true} options={['+234', '+322']} />
+    <div>
+      <Layout title="Sign Up" bg="bg-purple-600">
+        <div>
+          <div className="flex flex-col max-w-375 mx-auto">
+            <h4 className="text-2.5xl font-medium leading-tight mb-2 md:mb-2">
+              Create your account
+            </h4>
+            <p className="text-md text-gray-400">A short description about account types</p>
+            <form onSubmit={(e) => submit(e)} className="flex flex-col gap-6 mt-8">
+              <div>
+                <Input label="First name" />
               </div>
-              <div className="col-span-8">
-                <Input type="tel" label="Mobile number" block={true} />
+              <div>
+                <Input label="Last name" />
               </div>
-            </div>
-            <div>
-              <Input type="email" label="Email address" />
-            </div>
-            <Button loading={loading} block={true}>
-                Next
-            </Button>
-          </form>
+              <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-4">
+                  <Select label="Country" block={true} options={['+234', '+322']} />
+                </div>
+                <div className="col-span-8">
+                  <Input type="tel" label="Mobile number" block={true} />
+                </div>
+              </div>
+              <div>
+                <Input type="email" label="Email address" />
+              </div>
+              <Button loading={loading} block={true}>
+                  Next
+              </Button>
+            </form>
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { colors } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   // jit: true,
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './layouts/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
